967b9eff7dae051b64a71a54b3bfd22e13c1aedb
Antoine <sirpa.antoine@gmail.com>
2023-03-08 06:58:30 +0100

    add navbar with bootswatch

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app.home.index')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
