ed7ec0e878973b257ca466de62e9e3e3a8d184d2
Antoine <sirpa.antoine@gmail.com>
2023-03-09 10:31:56 +0100

    create fixture with paker php

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\IngredientRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -26,6 +27,11 @@ class Ingredient
 
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
