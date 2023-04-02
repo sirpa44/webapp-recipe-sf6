14d7ae9457b91cf583a31240f375bc8918727590
Antoine <sirpa.antoine@gmail.com>
2023-04-02 08:44:39 +0200

    recipe entity

---
+++ src/Entity/Recipe.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use DateTimeImmutable;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: RecipeRepository::class)]
+class Recipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\length(min: 2, max: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Assert\Range(
+        min: 1,
+        max: 1440,
+        notInRangeMessage: 'Time must be between {{ min }} and {{ max }}.',
+    )]
+    private ?int $time = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Assert\Range(
+        min: 1,
+        max: 50,
+        notInRangeMessage: 'People must be between {{ min }} and {{ max }}.',
+    )]
+    private ?int $people = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Assert\Range(
+        min: 1,
+        max: 5,
+        notInRangeMessage: 'Difficulty must be between {{ min }} and {{ max }}.',
+    )]
+    private ?int $difficulty = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank]
+    private ?string $description = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Assert\Range(
+        min: 1,
+        max: 1000,
+        notInRangeMessage: 'Price must be between {{ min }} and {{ max }}.',
+    )]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $creationDate = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $favorite = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $updateDate = null;
+
+    #[ORM\ManyToMany(targetEntity: ingredient::class)]
+    private Collection $ingredient;
+
+    public function __construct()
+    {
+        $this->creationDate = new DateTimeImmutable();
+        $this->ingredient = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTime(): ?int
+    {
+        return $this->time;
+    }
+
+    public function setTime(?int $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getPeople(): ?int
+    {
+        return $this->people;
+    }
+
+    public function setPeople(?int $people): self
+    {
+        $this->people = $people;
+
+        return $this;
+    }
+
+    public function getDifficulty(): ?int
+    {
+        return $this->difficulty;
+    }
+
+    public function setDifficulty(?int $difficulty): self
+    {
+        $this->difficulty = $difficulty;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeImmutable
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTimeImmutable $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function isFavorite(): ?bool
+    {
+        return $this->favorite;
+    }
+
+    public function setFavorite(?bool $favorite): self
+    {
+        $this->favorite = $favorite;
+
+        return $this;
+    }
+
+    public function getUpdateDate(): ?\DateTimeInterface
+    {
+        return $this->updateDate;
+    }
+
+    public function setUpdateDate(?\DateTimeInterface $updateDate): self
+    {
+        $this->updateDate = $updateDate;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ingredient>
+     */
+    public function getIngredient(): Collection
+    {
+        return $this->ingredient;
+    }
+
+    public function addIngredient(ingredient $ingredient): self
+    {
+        if (!$this->ingredient->contains($ingredient)) {
+            $this->ingredient->add($ingredient);
+        }
+
+        return $this;
+    }
+
+    public function removeIngredient(ingredient $ingredient): self
+    {
+        $this->ingredient->removeElement($ingredient);
+
+        return $this;
+    }
+}
